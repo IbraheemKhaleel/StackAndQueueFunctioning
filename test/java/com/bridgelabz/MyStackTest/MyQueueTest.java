da87c1a87199c3e7ae0b2f7e0f81841e05c430a0
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-28 12:00:41 +0530

    Added : code for testing dequeuing elements from queue

--- test/java/com/bridgelabz/MyStackTest/MyQueueTest.java
+++ test/java/com/bridgelabz/MyStackTest/MyQueueTest.java
@@ -13,24 +13,21 @@ public class MyQueueTest {
 		myQueue.enqueu(myFirstNode);
 		myQueue.enqueu(mySecondNode);
 		myQueue.enqueu(myThirdNode);
-		myQueue.printStack();
 		INode peak = myQueue.peak();
+		myQueue.printStack();
 		Assert.assertEquals(myFirstNode, peak);
 	}
 	@Test
-	public void givenValues_WhenDequeueFromQueue_ShouldRemoveFirstAddedNode() {
+	public void givenValues_WhenDeQueue_ShouldHaveFirstAddedNode() {
 		MyNode<Integer> myFirstNode = new MyNode<>(56);	
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode =  new MyNode<>(70);
-		MyStack myStack = new MyStack();
-		myStack.push(myFirstNode);
-		myStack.push(mySecondNode);
-		myStack.push(myThirdNode);	
-		INode pop = null ;
-		while(!myStack.isEmpty()) {
-			pop = myStack.pop();
-		}
-		Assert.assertEquals(myFirstNode, pop);	
+		MyStack myQueue = new MyStack();
+		myQueue.enqueu(myFirstNode);
+		myQueue.enqueu(mySecondNode);
+		myQueue.enqueu(myThirdNode);
+		INode dequeue = myQueue.dequeue();
+		myQueue.printStack();
+		Assert.assertEquals(myFirstNode, dequeue);
 	}
-
 }
